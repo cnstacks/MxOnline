ac56c08c8ea703e9528a2295090ef2ec00ecab02
tqtl911@163.com <19930911cXS>
2018-09-25 21:02:46 +0800

    修改

---
+++ apps/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+# Project: MxOnline 
+# Software: PyCharm
+# Time    : 2018-09-25 19:54
+# File    : __init__.py.py
+# Author  : 天晴天朗
+# Email   : tqtl@tqtl.org
+
+
