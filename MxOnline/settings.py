07189afcd06502ba9c0a8bd27675f1f5a87d4baf
tqtl911@163.com <19930911cXS>
2018-09-25 19:36:13 +0800

    4.9.1 operation 应用的创建； 4.9.2 operation/models.py的编写； 4.9.3 最后在settings.py中进行INSTALLED_APPS的注册；

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'courses',
+    'organization',
+    'operation',
 ]
 AUTH_USER_MODEL = "users.UserProfile"
 
