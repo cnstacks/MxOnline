eb98533ad9e5997ed9cefdc070df2cb8f83a9af8
tqtl911@163.com <19930911cXS>
2018-09-26 21:11:04 +0800

    login视图函数的编写；

---
+++ static/js/autocomplete-init.js
@@ -0,0 +1,14 @@
+
+function makeAutocomplete(sel) {
+    sel.autocomplete('/examples/autocomplete-products/', {
+        delay: 200,
+        formatItem: function(row) {
+            return row[1];
+        }
+    });
+}
+
+$(function() {
+    makeAutocomplete($('.autocomplete-me'));
+})
+ 
