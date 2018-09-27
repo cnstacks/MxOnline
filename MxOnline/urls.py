2c0c0ac76559d77c48de796b17d3ea530cc2a3c9
tqtl911@163.com <19930911cXS>
2018-09-27 16:40:49 +0800

    使用Django的form代替自定义函数login_user

--- MxOnline/urls.py
+++ MxOnline/urls.py
@@ -18,10 +18,10 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 import xadmin
 from users.views import LoginView
+#from users.views import LoginView
 
 urlpatterns = [
     url(r'^xadmin/', xadmin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="index.html"), name="index"),
-    url(r'^login/$', TemplateView.as_view(template_name="login.html"), name="login"),
-    # url('^login/$', LoginView.as_view(), name="login")
+    url('^login/$', LoginView.as_view, name="login")
 ]
--- apps/users/forms.py
+++ apps/users/forms.py
@@ -9,6 +9,6 @@
 from django import forms
 
 
-class LoginForm():
+class LoginForm(forms.Form):
     username = forms.CharField(required=True)
-    password = forms.CharField(required=True)
+    password = forms.CharField(required=True,min_length=5)
