c13dd53c580fedc8f86c14cf5c20e8f47eb086e2
tqtl911@163.com <19930911cXS>
2018-10-11 17:50:44 +0800

    Charfield必须执行max_length参数

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -9,26 +9,26 @@ from organization.models import CourseOrg, Teacher
 # Create your models here.
 
 # 创建四张表(类);
-
-
 class Course(models.Model):
     course_org = models.ForeignKey(CourseOrg, verbose_name=u"课程机构", null=True, blank=True)
     name = models.CharField(max_length=50, verbose_name=u"课程名")
     desc = models.CharField(max_length=300, verbose_name=u"课程描述")
     detail = models.TextField(verbose_name=u"课程详情")
-    degree = models.CharField(choices=(('cj', '初级'), ('zj', '中级'), ('gj', '高级')), max_length=100)
+    degree = models.CharField(max_length=100, choices=(('cj', '初级'), ('zj', '中级'), ('gj', '高级')))
     learn_times = models.IntegerField(default=0, verbose_name=u"学习时长(分钟数)")
     students = models.IntegerField(default=0, verbose_name=u"学习人数")
     fav_nums = models.IntegerField(default=0, verbose_name=u"收藏人数")
-    image = models.ImageField(upload_to="courses/%Y/%m", verbose_name=u"封面图", max_length=100)
+    image = models.ImageField(max_length=100, upload_to="courses/%Y/%m", verbose_name=u"封面图")
     click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
     class Meta:
         verbose_name = u"课程"
         verbose_name_plural = verbose_name
+
     def __unicode__(self):
         return self.name
+
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, verbose_name=u"课程")
