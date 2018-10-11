1a2cf63da02f6df9c503385cbcd9d59fad3cacfe
tqtl911@163.com <19930911cXS>
2018-10-11 18:22:18 +0800

    class Meta:         verbose_name = u"用户消息"         verbose_name_plural = verbose_name
            必须填写！

--- apps/operation/models.py
+++ apps/operation/models.py
@@ -44,7 +44,7 @@ class UserFavorite(models.Model):
 
 class UserMessage(models.Model):
     user = models.IntegerField(default=0, verbose_name=u"接收用户")
-    message = models.CharField(verbose_name=u"消息内容", max_length=500)
+    message = models.CharField(max_length=500, verbose_name=u"消息内容")
     has_read = models.BooleanField(default=False, verbose_name=u"是否已读")
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
 
